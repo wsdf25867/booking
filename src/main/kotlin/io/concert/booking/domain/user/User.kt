6d6a9cd756f5ea00fd95fd379b76c61532d71378
wsdf25867 <wsdf25867@gmail.com>
2024-10-17 00:29:37 +0900

    chore: User Entity & Table annotation 추가

--- src/main/kotlin/io/concert/booking/domain/user/User.kt
+++ src/main/kotlin/io/concert/booking/domain/user/User.kt
@@ -1,9 +1,9 @@
 package io.concert.booking.domain.user
 
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
+@Entity
+@Table(name = "users")
 class User(
     val name: String,
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
