2a108edfbdb57bb0f2c550fcb3bc7de0ad03dd2d
mito008 <ito.mbs3@kt.com>
2024-10-24 14:40:06 +0900

    fix : RestControllerAdvice 제거

--- src/main/kotlin/io/concert/booking/support/web/ControllerAdvice.kt
+++
@@ -1,8 +0,0 @@
-package io.concert.booking.support.web
-
-import org.springframework.web.bind.annotation.RestControllerAdvice
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
-
-@RestControllerAdvice
-class RestControllerAdvice: ResponseEntityExceptionHandler() {
-}
