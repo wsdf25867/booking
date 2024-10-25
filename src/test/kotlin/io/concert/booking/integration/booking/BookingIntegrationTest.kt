c7abd4d8698a64c0839d85eba30475425fa03f44
mito008 <ito.mbs3@kt.com>
2024-10-25 15:45:01 +0900

    test: 예약 동시성 테스트 추가

--- src/test/kotlin/io/concert/booking/integration/booking/BookingIntegrationTest.kt
+++ src/test/kotlin/io/concert/booking/integration/booking/BookingIntegrationTest.kt
@@ -6,21 +6,22 @@ import io.concert.booking.application.booking.dto.BookingDto
 import io.concert.booking.domain.booking.BookingRepository
 import io.concert.booking.domain.concert.Concert
 import io.concert.booking.domain.concert.ConcertRepository
+import io.concert.booking.domain.queue.Token
+import io.concert.booking.domain.queue.TokenRepository
 import io.concert.booking.domain.seat.Seat
 import io.concert.booking.domain.seat.SeatRepository
-import io.concert.booking.domain.user.User
-import io.concert.booking.domain.user.UserRepository
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.transaction.annotation.Transactional
+import org.springframework.data.jpa.repository.JpaRepository
 import java.time.LocalDateTime
-import java.util.*
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 
 @SpringBootTest
-@Transactional
 class BookingIntegrationTest {
 
     @Autowired
@@ -30,19 +31,16 @@ class BookingIntegrationTest {
     private lateinit var seatRepository: SeatRepository
 
     @Autowired
-    private lateinit var userRepository: UserRepository
+    private lateinit var tokenRepository: TokenRepository
 
     @Autowired
     private lateinit var bookingRepository: BookingRepository
 
     @Autowired
-    private lateinit var bookingService: BookingService
+    private lateinit var sut: BookingService
 
     @BeforeEach
     fun setUp() {
-        val user = User("some user")
-        userRepository.save(user)
-
         val concert = Concert("some concert", date = LocalDateTime.of(2999, 12, 31, 23, 59, 59))
         concertRepository.save(concert)
 
@@ -50,14 +48,54 @@ class BookingIntegrationTest {
         seatRepository.save(seat)
     }
 
+    @AfterEach
+    fun tearDown() {
+        (bookingRepository as JpaRepository<*, *>).deleteAllInBatch()
+        (seatRepository as JpaRepository<*, *>).deleteAllInBatch()
+    }
+
     @Test
     fun `예약을 만들 수 있다`() {
         // given
+        val token = Token(userId = 1, concertId = 1L)
+        tokenRepository.save(token)
         // when
-        val bookingDto: BookingDto = bookingService.create(BookingCreateDto(seatId = 1L, UUID.randomUUID().toString()))
+        val bookingDto: BookingDto = sut.create(BookingCreateDto(seatId = 1L, token.token.toString()))
 
         // then
         assertThat(bookingDto).extracting("bookingId", "seatId", "userId")
             .containsExactly(1L, 1L, 1L)
+    }
+
+    @Test
+    fun `한자리에 하나만 예약이 가능하다`() {
+        // given
+        val token = Token(userId = 1, concertId = 1L)
+        tokenRepository.save(token)
+        val count = 10
+        val letch = CountDownLatch(count)
+        val threadPool = Executors.newFixedThreadPool(count)
+        val results = mutableListOf<Boolean>()
+        // when
+        repeat(count) {
+            threadPool.execute {
+                try {
+                    sut.create(BookingCreateDto(seatId = 1L, token.token.toString()))
+                    results.add(true)
+                } catch (e: Exception) {
+                    results.add(false)
+                } finally {
+                    letch.countDown()
+                }
+            }
+        }
+        letch.await()
+
+        // then
+        val success = results.count { it }
+        val fail = results.count { !it }
+
+        assertThat(success).isEqualTo(1)
+        assertThat(fail).isEqualTo(9)
     }
 }
