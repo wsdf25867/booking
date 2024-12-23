ab0deda6739d3c207381a22737cc5952d18550ea
wsdf25867 <wsdf25867@gmail.com>
2024-12-23 11:43:45 +0900

    fix: h2-console 로그인 문제로 filter 삭제

--- src/main/kotlin/io/ryan/booking/BookingApplication.kt
+++ src/main/kotlin/io/ryan/booking/BookingApplication.kt
@@ -1,10 +1,8 @@
 package io.ryan.booking
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@EnableConfigurationProperties
 @SpringBootApplication
 class BookingApplication
 
